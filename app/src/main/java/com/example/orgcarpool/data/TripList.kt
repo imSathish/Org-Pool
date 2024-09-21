fd96791946e204dd7c86472fcae6e7ef9623e695
Sathishkumar <jsathishkumar4@gmail.com>
2024-09-21 15:13:47 +0530

    Pojo

---
+++ app/src/main/java/com/example/orgcarpool/data/TripList.kt
@@ -0,0 +1,22 @@
+package com.example.orgcarpool.data
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TripList(
+    @SerialName("trips") val trips: List<Trip>
+) {
+    @Serializable
+    data class Trip(
+        @SerialName("car") val car: String,
+        @SerialName("date") val date: String,
+        @SerialName("filled") val filled: Int,
+        @SerialName("from") val from: String,
+        @SerialName("owner") val owner: Int,
+        @SerialName("time") val time: String,
+        @SerialName("to") val to: String,
+        @SerialName("total_seats") val totalSeats: Int
+    )
+}
